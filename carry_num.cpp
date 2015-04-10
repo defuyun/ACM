0abb06a82c423e3b22edc244f4faa2cfd15565ed
Carl liu <carlliu2012@gmail.com>
2015-04-10 22:55:42 +1000

    added exit condition explaination

--- carry_num.cpp
+++ carry_num.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<iostream>
 // Q. calculate the number of times a carry in would occur for 2 number addition, the number would be < 9 digit
-// max int 2,147,483,647. > 2,000,00,00 so using int to store is fine
+// max int 2,147,483,647. > 2,000,00,00 so using int to store is fine, input 0 0 means end of file
 int main(){
 	int a,b,c = 0;
 	int ans = 0;
